9810294ac9d2985ca49ef59ab6aae0c482629384
Joseph Yoon <jyoonopop@gmail.com>
2022-08-13 11:50:34 -0400

    adding bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -5,6 +5,12 @@ function error404 () {
     return (
         <Def>
             <h1>404: PAGE NOT FOUND</h1>
+            <div>
+                <img src='/images/error-cat.jpg' alt='Looking Cat'/>
+                <div>
+                    Photo by <a href="https://unsplash.com/@madhatterzone?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Manja Vitolic</a> on <a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
+            </div>
             <p>Oops, sorry, we cannot find this page!</p>
         </Def>
     )
--- views/places/home.jsx
+++ views/places/home.jsx
@@ -6,12 +6,16 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src='/images/chia-fruit-drink.jpg' alt = 'Chia Fruit Shake' width='40%'/>
+                    <div>
+                        Photo by <a href="https://unsplash.com/es/@cravethebenefits?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Brenda Godinez</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
+                </div>
                 <a href="/places">
                     <button className="btn btn-primary">Places Page</button>
                 </a>
-
             </main>
-            
         </Def>
     )
 }
